607ea2e62112651387d9bfddcad4601a600685cb
Ayen <ayenmedinac@unimagdalena.edu.co>
2024-06-21 17:31:25 -0500

    add:Routes

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\SuperMercadoController;
 use App\Http\Controllers\Api\CiudadController;
 
-Route::get('/supermercado', [SuperMercadoController::class, 'getListaSuperMercado']);
+#Rutas del supermercado
+Route::get('/supermercado/all', [SuperMercadoController::class, 'index']);
+Route::post('/supermercado', [SuperMercadoController::class, 'store']);
+Route::get('/supermercado/{id}', [SuperMercadoController::class, 'show']);
+Route::delete('/supermercado/{id}', [SuperMercadoController::class, 'destroy']);
+Route::put('/supermercado/{id}', [SuperMercadoController::class, 'update']);
+
+#Rutas de la ciudad
+Route::post('/ciudad', [CiudadController::class, 'store']);
+Route::get('/ciudades', [CiudadController::class, 'index']);
